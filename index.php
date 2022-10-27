21a613af17bfa5aea7f5382fe50d80e8267c9cc6
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-10-27 19:37:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include "inc/header.php"; ?>
+    
+    <section class="body_part">
+       <div class="row">
+        <div class="col-md-12">
+        <div class="card text-center">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <div class="card-footer text-muted">
+               <div class="loading_section">
+                <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+          </div>
+            </div> 
+            </div>
+            </div>
+          </div>
+        </div>
+       </div>
+    </section>
+
+<?php include "inc/footer.php";?>
